55f5eaa15f302e2ec47ec2a99d6842ec69840e96
SheebuAnsari <ansari.sheebu786@gmail.com>
2024-01-13 14:36:12 +0530

    Accounts Ctrl Not in use

--- 07012024/MySchoolLatest/Focus8W/Focus8W/Focus8W/Controllers/CommonController.cs
+++ 07012024/MySchoolLatest/Focus8W/Focus8W/Focus8W/Controllers/CommonController.cs
@@ -1,40 +1,40 @@
-﻿using DomLibrary;
-using SchoolBones;
-using SchoolBones.Common;
+﻿//using DomLibrary;
+//using SchoolBones;
+//using SchoolBones.Common;
 
-using SchoolBones.Teacher;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+//using SchoolBones.Teacher;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
 
-namespace Focus8W.Controllers
-{
-    public class CommonController : Controller
-    {
-        TeacherDML oTeacherDML = new TeacherDML();
-        CommonDML oCommonDML = new CommonDML();
+//namespace Focus8W.Controllers
+//{
+//    public class CommonController : Controller
+//    {
+//        TeacherDML oTeacherDML = new TeacherDML();
+//        CommonDML oCommonDML = new CommonDML();
 
-        //public ActionResult FillUserDetails(int iRegId)
-        //{
-        //    Registration oRegistration = null;
-        //    oRegistration = oCommonDML.LoadRegisteredUser(iRegId);
-        //    if (oRegistration == null)
-        //    {
-        //        return Json("Alert : No record available.", JsonRequestBehavior.AllowGet);
-        //    }
-        //    else
-        //    {
-        //        return PartialView("_AddUser", oRegistration);
-        //    }
+//        //public ActionResult FillUserDetails(int iRegId)
+//        //{
+//        //    Registration oRegistration = null;
+//        //    oRegistration = oCommonDML.LoadRegisteredUser(iRegId);
+//        //    if (oRegistration == null)
+//        //    {
+//        //        return Json("Alert : No record available.", JsonRequestBehavior.AllowGet);
+//        //    }
+//        //    else
+//        //    {
+//        //        return PartialView("_AddUser", oRegistration);
+//        //    }
 
-        //}
-        public ActionResult LoadRegUserRefresh(int iModuleId)
-        {
-            DoubleIntValue oData = null;
-            oData = CommonMethod.LoadRegUser(iModuleId, (int)SubModule.Registration);
-            return PartialView("_Registration", oData);
-        }
-    }
-}
+//        //}
+//        public ActionResult LoadRegUserRefresh(int iModuleId)
+//        {
+//            DoubleIntValue oData = null;
+//            oData = CommonMethod.LoadRegUser(iModuleId, (int)SubModule.Registration);
+//            return PartialView("_Registration", oData);
+//        }
+//    }
+//}
